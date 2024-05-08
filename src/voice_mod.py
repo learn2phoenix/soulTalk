fe12a8d44286f7194b79eafc15a2ace7514b4a57
soumik-kanad <soumik.kanad@gmail.com>
2024-05-08 15:46:51 -0400

    Update voice_mod.py
    Changing multichannel audio to single channel audio.

--- src/voice_mod.py
+++ src/voice_mod.py
@@ -8,7 +8,11 @@ knn_vc = torch.hub.load('bshall/knn-vc', 'knn_vc', prematched=True, trust_repo=T
 def modulate(src, ref, out):
     """Modulates the source wav file to ref wav file. Both the files should be at 16kHz"""
     query_seq = knn_vc.get_features(src)
+    if len(list(query_seq.shape))>2:
+        query_seq = query_seq[0]
     matching_set = knn_vc.get_matching_set(ref)
+    if len(list(matching_set.shape))>2:
+        matching_set = matching_set[0]
 
     out_wav = knn_vc.match(query_seq, matching_set, topk=4)
     torchaudio.save(out, out_wav[None], 16000)
